390606899aa813d2757bd280ccf452155dc34cca
zaidannn7 <zaidanmovic0512@gmail.com>
2022-10-16 19:17:01 +0700

    juice: add cross compile

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -112,8 +112,10 @@ BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
 
 # Kernel Toolchain
 TARGET_KERNEL_CLANG_COMPILE := true
-KERNEL_TOOLCHAIN_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-cosmic
-TARGET_KERNEL_ADDITIONAL_FLAGS := LLVM=1 HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
+KERNEL_TOOLCHAIN_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/cosmic-clang
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
+TARGET_KERNEL_ADDITIONAL_FLAGS += LLVM=1
+ 
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
