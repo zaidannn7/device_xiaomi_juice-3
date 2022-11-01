a19c9c55723c0dfee686d1fcce1653335fb1ee86
zaidannn7 <72336864+zaidannn7@users.noreply.github.com>
2022-11-01 10:42:36 +0700

    juice: use TARGET_KERNEL_CROSS_COMPILE instead TARGET_KERNEL_CROSS_COMPILE_PREFIX  * and set it to true

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -112,8 +112,9 @@ BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
 
 # Kernel Toolchain
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := cosmic
 KERNEL_TOOLCHAIN_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/cosmic-clang
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
+TARGET_KERNEL_CROSS_COMPILE := aarch64-linux-gnu-
 TARGET_KERNEL_ADDITIONAL_FLAGS += LLVM=1
  
 
